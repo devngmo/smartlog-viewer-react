b86349db13a14e0147af123f6520a11e0baad181
Tran Manh Linh <devngmo@gmail.com>
2023-01-19 16:59:57 +0700

    update IssueItem

--- src/components/logviewer/IssueItem.js
+++ src/components/logviewer/IssueItem.js
@@ -31,11 +31,18 @@ class IssueItem extends React.Component {
                 data = JSON.stringify(this.props.issue.data, null, 4);
             }
         }
+        let tags = [];
+        if (this.props.issue.tags !== null && this.props.issue.tags !== undefined) {
+            for(let i = 0; i < this.props.issue.tags.length; i++) {
+                tags.push(<div key={i}>{this.props.issue.tags[i]}</div>);
+            }
+        }
       return <div 
         className={'issue-item ' + this.props.issue.v + (this.state.showData?' show-data':'')} onClick={() => this.toogleShowData()}>
         <div className='msg'>
-            <span className="tag">{this.props.issue.tag}</span> {this.props.issue.msg}            
+            <span className="mod">{this.props.issue.m}</span> {this.props.issue.msg}            
         </div>
+        <div className='tags'>{tags}</div>
         <div prep="code">{data}</div>
         <Moment date={this.props.issue.time} format="DD-MM-YYYY hh:mm:ss"></Moment>
       </div>;
